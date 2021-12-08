9f19c9ca6ce93283362bae612181f61d5d03f419
cmgbele <chriscyprian13@gmail.com>
2021-12-08 09:07:21 +0000

    initial commit

---
+++ PYTHON  CLASS.py
@@ -0,0 +1,67 @@
+First_integer = 99 
+#print(First_integer)
+
+my_verdict = True 
+print(my_verdict)
+
+random_items =("sports", "spider", True,"false")
+#print(random_items)
+
+new_world = "Strength"
+print(new_world[3]) 
+
+mini_string = "Blacklist"
+max_string ="The {} is getting tiresome".format(mini_string)
+print(max_string)
+
+mod_string ="The nation\'s crude oil"
+print(mod_string)
+
+
+source_of_income = 'The nation\'s crude oil'
+print("source_of_code") 
+
+new_weather_report = "It is quite sunny despite the downpour all through the night"
+mod_one = new_weather_report.title()
+#print(mod_on
+
+mod_two  = new_weather_report.startswith("It")
+print(mod_two)
+
+mod_three = new_weather_report.index("n")
+# print(mod_three) 
+
+mod_four = new_weather_report.split(" downpour ")
+#print(mod_four)
+
+mod_five =" ".join(mod_four)
+#print(mod_five) 
+
+mod_six = new_weather_report.count("the")
+#print(mod_six)
+
+mod_seven = new_weather_report.replace("p","f")
+#print(mod_seven)
+
+
+mopd_eight = new_weather_report.strip("It is")
+#print(mod_eight)
+
+new_colection = [6, True, 12, 9, False, "name", "Bentltley","Football",9.7, None, 8.5]
+
+desired_item = new_collection[4]
+#print(desired_item)
+
+desired_portiion = new_collection[-6:-2:2]
+#print(desired_portion)
+
+
+sports = ["Basketball", "Boxing" , "Swimmimng"]
+stars = ["Kobe Byant" , "Mike tyson", "Michael phelps"]
+
+sports.extend(stars)
+# print(sports)
+
+###TUPLES
+scores = (56,73,85,67)
+desired_score = sco
---
+++ private (2).py
@@ -0,0 +1,26 @@
+print("Welcome To CHRISBANK")
+Trials = 3
+Userpin = 1234 
+
+while Trials != 0:
+    pin = int(input("Please Enter Your 4 digit pin Number: :")) 
+    if pin != Userpin:
+        Trials -= 1 
+        print("Wrong pin Number,You Have,Trials", Trials,"Trials Left")
+    else: 
+         Userchoice = input("d:Deposit or w: Withdraw: ")
+         if Userchoice == "d": 
+                UserDeposit = input("Enter TheAmount You Would like to Deposit: ") 
+                print(UserDeposit, "$ Have Been Deposited Into your Account") 
+         if Userchoice == "u": 
+                UserDeposit = input("Enter TheAmount You Would like to withdraw: ") 
+                print(UserDeposit, "$ Have Been withdrawn Into your Account") 
+UserExit = input("Would You Like To Continue? Y/N: ") 
+if UserExit == "N":
+    print("Thank You For Using CHRISBANK ")
+    break     
+else: 
+    continue 
+    
+
+
