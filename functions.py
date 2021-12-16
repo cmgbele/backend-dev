42e17cce1f82d89ec404585a622167acb6c8035d
cmgbele <chriscyprian13@gmail.com>
2021-12-16 08:44:07 +0000

    initial commit

---
+++ functions.py
@@ -0,0 +1,54 @@
+# var_ = yes
+# var_1 = yes 
+def newline():
+    print('.') 
+
+
+# def three_line():
+#     newline()
+#     newline()
+#     newline()
+
+
+# three_line()
+
+# def ninelines():
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+#     newline()
+# def twentyfivelines():
+
+
+
+# # ninelines()
+# def add_valaie
+#     def orifinal - result = func
+#     miodiofieres = 
+
+
+user ={} 
+
+def define(func):
+    def inner():
+        username = input("username\n>")
+        password = input("Password\n>")
+
+        if user.get(username) != None and user
